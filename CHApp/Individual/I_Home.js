37383ab0b5a1acb712ea68b9efef6e11bdf2040a
Malak Soubai <smalak@ad.unc.edu>
2023-08-17 21:18:13 -0400

    const Card

--- CHApp/Individual/I_Home.js
+++ CHApp/Individual/I_Home.js
@@ -10,6 +10,23 @@ import { Entypo } from '@expo/vector-icons';
 
 import styles from '../Style.js';
 
+const Card = () => {
+  return (
+    <View style={styles.cards}>
+      <View style={styles.leftColumn}>
+        <Text style={styles.clubName}>Your Club Name</Text>
+        <Text style={styles.eventTitle}>Event Title</Text>
+        <Text style={styles.notice}>Volunteers Needed</Text>
+      </View>
+      <View style={styles.rightColumn}>
+        <Text style={styles.date}>Event Date</Text>
+        <Text style={styles.location}>Event Location</Text>
+        <Text style={styles.teaser}>Event Teaser</Text>
+      </View>
+    </View>
+  );
+};
+
 const I_HomeScreen = () => {
     return(
       <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={10}>
@@ -19,22 +36,16 @@ const I_HomeScreen = () => {
           <View style={styles.profileIcon}>
           <Ionicons name="person-circle" size={50} color="#13294B" onPress={() => navigation.navigate('I_Profile')}/>
           </View>
-          <Text style={styles.user}>John Doe</Text>
-          <View style={styles.cards}>
-            <Text>This is a post</Text>
-          </View>
-          <View style={styles.cards}>
-            <Text>This is a post</Text>
-          </View>
-          <View style={styles.cards}>
-            <Text>This is a post</Text>
-          </View>
-          <View style={styles.cards}>
-            <Text>This is a post</Text>
-          </View>
-          <View style={styles.cards}>
-            <Text>This is a post</Text>
-          </View>
+          <Text style={styles.user}>Welcome, Guest</Text>
+          {/* <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Volunteer</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Donate</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>History</Text>
+          </TouchableOpacity> */}
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
